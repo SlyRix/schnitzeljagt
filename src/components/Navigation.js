4f6b348d423175302ea10d3595742f5436ed43da
SlyRix <rushelsilvester@outlook.com>
2025-08-04 10:13:56 +0200

    Re-enable navigation to previous station and set DEV_MODE to true for debugging purposes.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Navigation = ({ station, onNavigate, isFinal = false }) => {
     return (
         <div className="navigation">
-            {/*{station.prevStation && (*/}
-            {/*    <button className="nav-button" onClick={() => onNavigate(station.prevStation)}>*/}
-            {/*        ← Zurück*/}
-            {/*    </button>*/}
-            {/*)}*/}
+            {station.prevStation && (
+                <button className="nav-button" onClick={() => onNavigate(station.prevStation)}>
+                    ← Zurück
+                </button>
+            )}
             {station.nextStation && (
                 <button className="nav-button" onClick={() => onNavigate(station.nextStation)}>
                     {station.nextStation === 'essen' ? 'Zum Finale! →' : 'Weiter! →'}
